e2f23aa7cd6cda44ed2c1843fabe86508bdd7a88
rcraig <ryanpcraig1@gmail.com>
2021-10-06 20:18:28 -0500

    cupdated my project cards

--- client/src/components/Projects.js
+++ client/src/components/Projects.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useEffect, useState } from 'react';
 import { featuredPortfolio } from './data';
+import { Card, Container } from 'react-bootstrap';
 
 export default function Projects() {
     const [data, setData] = useState([]);
@@ -11,17 +12,30 @@ export default function Projects() {
     
   return (
     <div className="projects" id="projects">
+      {/* <Container> */}
       <h1>Projects</h1>
       <div className="project-container">
         {data.map((d) => (
-          <a href={d.gitLink}>
-            <div className="project-item">
-              <img src={d.img} alt=""/>
-              <h3>{d.title}</h3>
+          // <a href={d.gitLink}>
+            <div className="project-card">
+              <Card style={{ width: '18rem' }}>
+              <div className="project-item">
+                <img src={d.img} alt=""/>
+                {/* <h3>{d.title}</h3> */}
+              </div>
+                <Card.Body>
+                  <Card.Title>{d.title}</Card.Title>
+                  <Card.Text>{d.desc}</Card.Text>
+                  <Card.Link href={d.gitLink}>GitHub</Card.Link>
+                  <Card.Link href="#">Deployed Link</Card.Link>
+                </Card.Body>
+              </Card>
             </div>
-          </a>
+            
+          // </a>
         ))}
       </div>
+      {/* </Container> */}
     </div>
   )
 }
--- client/src/components/data.js
+++ client/src/components/data.js
@@ -3,36 +3,42 @@ export const featuredPortfolio = [
     id: 1,
     title: "Snake",
     img: "assets/SnakeGame.gif",
-    gitLink: "https://github.com/Rpcraig123/unit1_snake"
+    gitLink: "https://github.com/Rpcraig123/unit1_snake",
+    desc: "A recreation of an arcade classic written in HTML, CSS, and Javascript. Deployed with Surge."
   },
   {
     id: 2,
     title: "Credit Card Debt Tracker",
     img: "assets/CreditCard.gif",
-    gitLink: "https://github.com/Rpcraig123/unit2_budget_app"
+    gitLink: "https://github.com/Rpcraig123/unit2_budget_app",
+    desc: "A M.E.R.N stack appliction where a user can keep track of their credit card debt."
   },
   {
     id: 3,
     title: "FilmZilla",
     img: "assets/FilmZilla.gif",
-    gitLink: "https://github.com/Rpcraig123/Filmzilla"
+    gitLink: "https://github.com/Rpcraig123/Filmzilla",
+    desc: "A P.E.R.N stack application where users can dicover new movies and review their favorites."
   },
   {
     id: 4,
     title: "Tic Tac Toe",
     img: "assets/TicTacToe.gif",
-    gitLink: "https://github.com/Rpcraig123/u1_hw_tic_tac_toe"
+    gitLink: "https://github.com/Rpcraig123/u1_hw_tic_tac_toe",
+    desc: "Play Tic Tac Toe in this simple web app built with HTML, CSS, and Javascript. Deployed with Surge."
   },
   {
     id: 5,
     title: "RawG Video Game App",
     img: "assets/RawG1.gif",
-    gitLink: "https://github.com/Rpcraig123/u4_hw_vue_RAWG"
+    gitLink: "https://github.com/Rpcraig123/u4_hw_vue_RAWG",
+    desc: "Discover new video games with this RawG API powered web app. Created with Vue.js"
   },
   {
     id: 6,
     title: "Vue Messenger",
     img: "assets/messenger.gif",
-    gitLink: "https://github.com/Rpcraig123/u4_hw_vue_messenger"
+    gitLink: "https://github.com/Rpcraig123/u4_hw_vue_messenger",
+    desc: "An messaging web app used to communicate with other students. Built using Vue.js and websockets"
   }
 ];
